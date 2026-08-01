03d1a1a80b10d2fe206d7d77331c837117d8c19d
dhruv-dobariya <dhruvdobariya157@gmail.com>
2026-08-01 13:30:02 +0530

    Fix all instances of getTotalCartAmount in Navbar

--- frontend/src/Componet/Navbar/Navbar.jsx
+++ frontend/src/Componet/Navbar/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = ({ setShowLogin }) => {
                     <a href='#explore-menu' onClick={() => { setMenu("menu"); closeMobile() }}>🍽️ Menu</a>
                     <a href='#app-download' onClick={() => { setMenu("mobile-app"); closeMobile() }}>📱 Mobile App</a>
                     <a href='#footer' onClick={() => { setMenu("contact-us"); closeMobile() }}>📞 Contact</a>
-                    <Link to='/cart' onClick={closeMobile}>🛒 Cart {getTotalCartAmount() > 0 && `($${getTotalCartAmount()})`}</Link>
+                    <Link to='/cart' onClick={closeMobile}>🛒 Cart {getToatalCartAmount() > 0 && `($${getToatalCartAmount()})`}</Link>
                     {token
                         ? <>
                             <span onClick={() => { navigate('/myorders'); closeMobile() }}>📦 My Orders</span>
